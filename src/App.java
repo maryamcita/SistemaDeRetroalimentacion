c18622da5713fe8183cdd0f5e624451f29a9f5ff
maryamcita <jukiluma000@gmail.com>
2025-04-30 18:03:00 -0500

    terminacion de las encuestas

--- src/App.java
+++ src/App.java
@@ -42,5 +42,105 @@ public class App {
                 System.out.println("la nota es: *****");
                 break;
         }
+        System.out.println("Entrega retroalimentación oportuna sobre el desempeño académico");
+        System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
+        String opcion3 = sc.nextLine();    
+        switch (opcion3) {
+            case "1":
+                System.out.println("la nota es: *");
+                break;
+            case "2":
+                System.out.println("la nota es: **");
+                break;
+            case "3":
+                System.out.println("la nota es: ***");
+                break;
+            case "4":
+                System.out.println("la nota es: ****");
+                break;
+            case "5":
+                System.out.println("la nota es: *****");
+                break;
+        }
+        System.out.println("Muestra compromiso con el proceso educativo");
+        System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
+        String opcion4 = sc.nextLine();    
+        switch (opcion4) {
+            case "1":
+                System.out.println("la nota es: *");
+                break;
+            case "2":
+                System.out.println("la nota es: **");
+                break;
+            case "3":
+                System.out.println("la nota es: ***");
+                break;
+            case "4":
+                System.out.println("la nota es: ****");
+                break;
+            case "5":
+                System.out.println("la nota es: *****");
+                break;
+        }
+        System.out.println("Demuestra dominio del tema o materia que imparte");
+        System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
+        String opcion5 = sc.nextLine();    
+        switch (opcion5) {
+            case "1":
+                System.out.println("la nota es: *");
+                break;
+            case "2":
+                System.out.println("la nota es: **");
+                break;
+            case "3":
+                System.out.println("la nota es: ***");
+                break;
+            case "4":
+                System.out.println("la nota es: ****");
+                break;
+            case "5":
+                System.out.println("la nota es: *****");
+                break;
+        }
+        System.out.println("Es puntual y cumple con los horarios establecidos");
+        System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
+        String opcion6 = sc.nextLine();    
+        switch (opcion6) {
+            case "1":
+                System.out.println("la nota es: *");
+                break;
+            case "2":
+                System.out.println("la nota es: **");
+                break;
+            case "3":
+                System.out.println("la nota es: ***");
+                break;
+            case "4":
+                System.out.println("la nota es: ****");
+                break;
+            case "5":
+                System.out.println("la nota es: *****");
+                break;
+        }
+        System.out.println("En general, estoy satisfecho/a con el desempeño del docente");
+        System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
+        String opcion7 = sc.nextLine();    
+        switch (opcion7) {
+            case "1":
+                System.out.println("la nota es: *");
+                break;
+            case "2":
+                System.out.println("la nota es: **");
+                break;
+            case "3":
+                System.out.println("la nota es: ***");
+                break;
+            case "4":
+                System.out.println("la nota es: ****");
+                break;
+            case "5":
+                System.out.println("la nota es: *****");
+                break;
+        }
         }
 }
