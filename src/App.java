fc28b4bcb8bb8c4bf77423eb0f57ddc4cdb193ba
maryamcita <jukiluma000@gmail.com>
2025-05-06 04:10:42 -0500

    Logica de preguntas y respuestas terminada por completo

--- src/App.java
+++ src/App.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);  
-        System.out.println("Evaluación y retroalimentación:\nInforma claramente los criterios de evaluación");
+        System.out.println("A) Metodologías y estrategias de enseñanza\n 1) ¿El docente presenta contenidos de manera clara y estructurada?");
         System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
-        String opcion = sc.nextLine();    
+        String opcion = sc.nextLine();
         switch (opcion) {
             case "1":
                 System.out.println("la nota es: *");
@@ -22,10 +22,210 @@ public class App {
                 System.out.println("la nota es: *****");
                 break;
         }
-        System.out.println("Las evaluaciones están relacionadas con los contenidos enseñados.");
+        System.out.println("2) ¿Utiliza recursos didadcticos adecuados (precentacions, videos, practicas, etc.?)");
         System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
-        String opcion2 = sc.nextLine();    
+        String opcion2 = sc.nextLine();
         switch (opcion2) {
+            case "1":
+                System.out.println("la nota es: *");
+                break;
+            case "2":
+                System.out.println("la nota es: **");
+                break;
+            case "3":
+                System.out.println("la nota es: ***");
+                break;
+            case "4":
+                System.out.println("la nota es: ****");
+                break;
+            case "5":
+                System.out.println("la nota es: *****");
+                break;
+        }
+        System.out.println("3) ¿Promueve la participacion activa de los estudiantes durante la clase?");
+        System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
+        String opcion3 = sc.nextLine();
+        switch (opcion3) {
+            case "1":
+                System.out.println("la nota es: *");
+                break;
+            case "2":
+                System.out.println("la nota es: **");
+                break;
+            case "3":
+                System.out.println("la nota es: ***");
+                break;
+            case "4":
+                System.out.println("la nota es: ****");
+                break;
+            case "5":
+                System.out.println("la nota es: *****");
+                break;
+        }
+        System.out.println("4) ¿Relaciona los contenidos con aplicaciones reales o del entorno técnico?.");
+        System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
+        String opcion4 = sc.nextLine();
+        switch (opcion4) {
+            case "1":
+                System.out.println("la nota es: *");
+                break;
+            case "2":
+                System.out.println("la nota es: **");
+                break;
+            case "3":
+                System.out.println("la nota es: ***");
+                break;
+            case "4":
+                System.out.println("la nota es: ****");
+                break;
+            case "5":
+                System.out.println("la nota es: *****");
+                break;
+        }
+        System.out.println("5) ¿Estimula el pensamiento crítico y la resolución de problemas?.");
+        System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
+        String opcion5 = sc.nextLine();
+        switch (opcion5) {
+            case "1":
+                System.out.println("la nota es: *");
+                break;
+            case "2":
+                System.out.println("la nota es: **");
+                break;
+            case "3":
+                System.out.println("la nota es: ***");
+                break;
+            case "4":
+                System.out.println("la nota es: ****");
+                break;
+            case "5":
+                System.out.println("la nota es: *****");
+                break;
+        }
+        System.out.println("B). Comunicación y Relación con el Estudiante \n6) ¿Estimula el pensamiento crítico y la resolución de problemas?.");
+        System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
+        String opcion6 = sc.nextLine();
+        switch (opcion6) {
+            case "1":
+                System.out.println("la nota es: *");
+                break;
+            case "2":
+                System.out.println("la nota es: **");
+                break;
+            case "3":
+                System.out.println("la nota es: ***");
+                break;
+            case "4":
+                System.out.println("la nota es: ****");
+                break;
+            case "5":
+                System.out.println("la nota es: *****");
+                break;
+        }
+        System.out.println("B). Comunicación y Relación con el Estudiante\n 6) ¿Estimula el pensamiento crítico y la resolución de problemas?.");
+        System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
+        String opcion7 = sc.nextLine();
+        switch (opcion7) {
+            case "1":
+                System.out.println("la nota es: *");
+                break;
+            case "2":
+                System.out.println("la nota es: **");
+                break;
+            case "3":
+                System.out.println("la nota es: ***");
+                break;
+            case "4":
+                System.out.println("la nota es: ****");
+                break;
+            case "5":
+                System.out.println("la nota es: *****");
+                break;
+        }
+        System.out.println("8) ¿Se comunica de manera respetuosa y profesional.?.");
+        System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
+        String opcion8 = sc.nextLine();
+        switch (opcion8) {
+            case "1":
+                System.out.println("la nota es: *");
+                break;
+            case "2":
+                System.out.println("la nota es: **");
+                break;
+            case "3":
+                System.out.println("la nota es: ***");
+                break;
+            case "4":
+                System.out.println("la nota es: ****");
+                break;
+            case "5":
+                System.out.println("la nota es: *****");
+                break;
+        }
+        System.out.println("9) ¿Escucha y responde adecuadamente a las preguntas de los estudiantes?.");
+        System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
+        String opcion9 = sc.nextLine();
+        switch (opcion9) {
+            case "1":
+                System.out.println("la nota es: *");
+                break;
+            case "2":
+                System.out.println("la nota es: **");
+                break;
+            case "3":
+                System.out.println("la nota es: ***");
+                break;
+            case "4":
+                System.out.println("la nota es: ****");
+                break;
+            case "5":
+                System.out.println("la nota es: *****");
+                break;
+        }
+        System.out.println("10) ¿Está disponible para atender consultas fuera del horario de clase?.");
+        System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
+        String opcion10 = sc.nextLine();
+        switch (opcion10) {
+            case "1":
+                System.out.println("la nota es: *");
+                break;
+            case "2":
+                System.out.println("la nota es: **");
+                break;
+            case "3":
+                System.out.println("la nota es: ***");
+                break;
+            case "4":
+                System.out.println("la nota es: ****");
+                break;
+            case "5":
+                System.out.println("la nota es: *****");
+                break;
+        }
+        System.out.println("Evaluación y retroalimentación:\nInforma claramente los criterios de evaluación");
+        System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
+        String opcion11 = sc.nextLine();    
+        switch (opcion11) {
+            case "1":
+                System.out.println("la nota es: *");
+                break;
+            case "2":
+                System.out.println("la nota es: **");
+                break;
+            case "3":
+                System.out.println("la nota es: ***");
+                break;
+            case "4":
+                System.out.println("la nota es: ****");
+                break;
+            case "5":
+                System.out.println("la nota es: *****");
+                break;
+        }
+        System.out.println("Las evaluaciones están relacionadas con los contenidos enseñados");
+        System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
+        String opcion12 = sc.nextLine();    
+        switch (opcion12) {
             case "1":
                 System.out.println("la nota es: *");
                 break;
@@ -44,8 +244,8 @@ public class App {
         }
         System.out.println("Entrega retroalimentación oportuna sobre el desempeño académico");
         System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
-        String opcion3 = sc.nextLine();    
-        switch (opcion3) {
+        String opcion13 = sc.nextLine();    
+        switch (opcion13) {
             case "1":
                 System.out.println("la nota es: *");
                 break;
@@ -64,8 +264,8 @@ public class App {
         }
         System.out.println("Muestra compromiso con el proceso educativo");
         System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
-        String opcion4 = sc.nextLine();    
-        switch (opcion4) {
+        String opcion14 = sc.nextLine();    
+        switch (opcion14) {
             case "1":
                 System.out.println("la nota es: *");
                 break;
@@ -84,8 +284,8 @@ public class App {
         }
         System.out.println("Demuestra dominio del tema o materia que imparte");
         System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
-        String opcion5 = sc.nextLine();    
-        switch (opcion5) {
+        String opcion15 = sc.nextLine();    
+        switch (opcion15) {
             case "1":
                 System.out.println("la nota es: *");
                 break;
@@ -104,7 +304,7 @@ public class App {
         }
         System.out.println("Es puntual y cumple con los horarios establecidos");
         System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
-        String opcion6 = sc.nextLine();    
+        String opcion16 = sc.nextLine();    
         switch (opcion6) {
             case "1":
                 System.out.println("la nota es: *");
@@ -124,7 +324,7 @@ public class App {
         }
         System.out.println("En general, estoy satisfecho/a con el desempeño del docente");
         System.out.println("1 - *\n2 - **\n3 - ***\n4 - ****\n5 - *****");
-        String opcion7 = sc.nextLine();    
+        String opcion17 = sc.nextLine();    
         switch (opcion7) {
             case "1":
                 System.out.println("la nota es: *");
